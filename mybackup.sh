863a3263d39688424f46a2b2245bf07011f04762
zico731 <91086783+zico731@users.noreply.github.com>
2022-10-29 01:24:04 +0200

    amélioration de la mise en page de la journalisation

--- mybackup.sh
+++ mybackup.sh
@@ -17,12 +17,12 @@ SOURCES=(
 '/mnt/c/Users/Paco/AppData/Roaming/Thunderbird'
 '/mnt/c/Users/Paco/Applications'
 '/mnt/c/Users/Paco/Developpement'
-'/mnt/c/Users/Paco/Documents/Virtual Machines'
+#'/mnt/c/Users/Paco/Documents/Virtual Machines'
 )
 NB_ITEMS=${#SOURCES[*]}
 
 ## REPERTOIRE de  destination
-DESTINATION='/mnt/f/mybackup/'
+DESTINATION='/mnt/d/mybackup/'
 
 
 # Un nom de dossier avec le quantième du jour
@@ -44,19 +44,22 @@ echo "  La sauvegarde sera faîte dans le dossier ${DESTINATION}."
 printf "  La synchronisation de "; for i in  $(seq 0 $((NB_ITEMS-1))); do echo -n "${SOURCES[$i]//*\/}, "; done | sed 's/..$/ commence:/'
 echo
 
+## creation d'une chaine 80 points servant à la mise en page
+POINTS=$(printf ".%.s" $(seq 60))
 
 for i in ${!SOURCES[*]}
 do 
 	echo "  ($((i+1))/${NB_ITEMS}) - ${SOURCES[i]//*\/} ..."
 	rsync -a --delete "${SOURCES[i]}" "${DESTINATION}" >> /var/log/mybackup.log 2>>/var/log/mybackup.err
-	
+	echo -e "${POINTS}#\r  ${SOURCES[i]}"|sed "s/#/[ok]/" >> /var/log/mybackup.log 2>>/var/log/mybackup.err
 done
 
-#echo "  La sauvegarde est terminée (duréé ${SECONDS}s) "
+## on stocke dans une variable la durée en secondes
 secs=$SECONDS  
-printf '  Durée: %dh:%02dm:%02ds ' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)) | tee -a /var/log/mybackup.log 
-echo "- Taille totale: $(du -sh /mnt/f/mybackup|cut -f1)" | tee -a /var/log/mybackup.log
-echo "" >> /var/log/mybackup.log 
+
+## on converti la duréée en Heure:Minute:Seconde
+echo -n "  Taille totale: $(du -sh /mnt/f/mybackup|cut -f1) - " | tee -a /var/log/mybackup.log
+printf 'Durée: %dh:%02dm:%02ds\n\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)) | tee -a /var/log/mybackup.log
 
 # La sauvegarde est notée dans le journal d'évènements du système :
 echo "  La sauvegarde est renseignée dans /var/log/mybackup.log"
